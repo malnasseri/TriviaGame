8f35a04d7c62e27651b122f947d834f245897c7f
Mohammed Alnasseri <mohammedalnasseri@Mohammeds-MacBook-Pro.local>
2018-02-26 23:25:15 -0500

    added some features and images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -134,7 +134,7 @@ function newQuestion(){
 }
 
 function countdown(){
-	seconds = 10;
+	seconds = 15;
 	$('#timeLeft').html('<h1>' + seconds + '</h1>');
 	answered = true;
 	//sets timer to go down
